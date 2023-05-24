2f852d9bf34143bd1e7324151fe9a26cb308e7ba
Bas Heerschop <heerschop@gmail.com>
2023-05-24 07:43:03 +0200

    webmanifest

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,42 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        theme_color: '#383a3f',
+        background_color: '#383a3f',
+        orientation: 'portrait',
+        id: 'push-app',
+        name: 'Push app',
+        short_name: 'Push app',
+        description: 'App for testing push notifications',
+        icons: [
+          {
+            'src': 'icons/icon-16x16.png',
+            'sizes': '16x16',
+            'type': 'image/png',
+          },
+          {
+            'src': 'icons/icon-32x32.png',
+            'sizes': '32x32',
+            'type': 'image/png',
+          },
+          {
+            'src': 'icons/icon-128x128.png',
+            'sizes': '128x128',
+            'type': 'image/png',
+          },
+          {
+            'src': 'icons/icon-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png',
+          },
+          {
+            'src': 'icons/icon-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png',
+          },
+        ],
+      },
     }),
   ],
 });
